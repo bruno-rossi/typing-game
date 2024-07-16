74b4bd5de5b898e2793bf0d46ce39f083c339ec6
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-07-16 17:51:58 -0400

    Added types and worked on new player modal

--- typing-game/src/App.tsx
+++ typing-game/src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { Player } from './types/types';
 
 function App() {
+
+  const [ player, setPlayer ] = useState<Player | null>(null);
+
   return (
     <div className="App">
         <NavBar />
-        <Outlet />
+        <Outlet context={{player: player, setPlayer: setPlayer}} />
         <Footer />
     </div>
   );
